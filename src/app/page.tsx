c89180e216f57abf1d846a6b1441bb29cc2687db
Vidyun Agarwal <vidyunagarwal@gmail.com>
2024-06-12 16:50:17 +0530

    initial setup of project

---
+++ src/app/page.tsx
@@ -0,0 +1,38 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { api } from "@/convex/_generated/api";
+import { SignOutButton, SignedIn, SignedOut } from "@clerk/clerk-react";
+import { SignInButton } from "@clerk/nextjs";
+import { useMutation, useQuery } from "convex/react";
+// import Image from "next/image";
+
+export default function Home() {
+  const Files = useQuery(api.files.getFiles);
+  const createFile = useMutation(api.files.createFile);
+  
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <SignedIn>
+          <SignOutButton> 
+             
+            </SignOutButton>
+        </SignedIn>
+        <SignedOut>
+        <SignInButton mode="modal" >
+        <Button>Sign In</Button>
+        </SignInButton>
+        </SignedOut>
+
+        {Files?.map(file => {
+          return <div key={file._id}> {file.name} </div>
+        })}
+        
+        <Button onClick={() => {
+          createFile({
+            name: 'hello world'
+          })
+        }}>Click Me</Button>
+    </main>
+  );
+}
